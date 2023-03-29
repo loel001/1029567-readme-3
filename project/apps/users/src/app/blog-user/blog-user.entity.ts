20a9bbc80c8b26088ec9a8addc60d22a4761886a
Podkasik Anna <ostretsova.ann@gmail.com>
2023-03-29 21:05:46 +0300

    add comparePassword

--- project/apps/users/src/app/blog-user/blog-user.entity.ts
+++ project/apps/users/src/app/blog-user/blog-user.entity.ts
@@ -1,5 +1,5 @@
 import { User } from '@project/shared/app-types';
-import { genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 import { SALT_ROUNDS } from './blog-user.constant';
 
 export class BlogUserEntity implements User {
@@ -37,5 +37,9 @@ export class BlogUserEntity implements User {
     const salt = await genSalt(SALT_ROUNDS);
     this.passwordHash = await hash(password, salt);
     return this;
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
   }
 }
