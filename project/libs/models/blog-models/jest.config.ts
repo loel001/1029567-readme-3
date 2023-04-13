22eb23bdbf1947afd7c9ce793ebfdf2b2bf02a27
Podkasik Anna <ostretsova.ann@gmail.com>
2023-04-13 21:18:01 +0300

    add models/blog-models

---
+++ project/libs/models/blog-models/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'models-blog-models',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/models/blog-models',
+};
